554e542ec4af2fdde5faaca3e6403b5be303f7da
GuiEloi <gcunhaeloi@gmail.com>
2017-08-31 19:28:47 -0300

    initial

--- app.js
+++ app.js
@@ -26,25 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
 // Sitemap constructor
 
 var sitemap = sm.createSitemap ({
@@ -66,5 +47,26 @@ app.get('/sitemap.xml', function(req, res) {
     });
 });
 // End of Sitemap constructor
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
 
 module.exports = app;
